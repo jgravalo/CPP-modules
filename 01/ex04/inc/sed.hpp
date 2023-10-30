e9130df46bc25bb496abca233eb83a28da3bdb9f
jgravalo <jesusgrawork361@gmail.com>
2023-10-30 12:11:52 +0100

    ex04 finalizado

---
+++ 01/ex04/inc/sed.hpp
@@ -0,0 +1,7 @@
+#ifndef SED_HPP
+#define SED_HPP
+
+#include<iostream>
+#include<fstream>
+
+#endif
--- 01/ex04/src/main.cpp
+++ 01/ex04/src/main.cpp
@@ -1,14 +1,7 @@
-#include<iostream>
-#include<fstream>
+#include<sed.hpp>
 
 int main(int argc, char **argv)
 {
-
-    if (argc != 4 || !argv)
-    {
-        std::cout << "Numero incorrecto de argumentos" << std::endl;
-        return (1);
-    }
     std::string     file = argv[1];
     std::string     input = argv[2];
     std::string     output = argv[3];
@@ -17,11 +10,16 @@ int main(int argc, char **argv)
     std::string     buffer;
     std::string     text;
 
+    if (argc != 4 || !argv)
+    {
+        std::cout << "Numero incorrecto de argumentos" << std::endl;
+        return (1);
+    }
     infile.open(file);
-    outfile.open(file + ".replace");//, std::ios::out);
+    outfile.open(file + ".replace");
     if (!infile.is_open() || !outfile)
     {
-        std::cout << "Error al abrir ejemplo.dat" << std::endl;
+        std::cout << "Error al abrir " << std::endl;
         return (1);
     }
     while (std::getline(infile, buffer))
@@ -36,14 +34,14 @@ int main(int argc, char **argv)
     {
         if (i == found)
         {
-            //std::cout << output;
             outfile << output;
             i += len - 1;
             found = text.find(input, i);
         }
         else
-            //std::cout << text[i];
             outfile << text[i];
     }
+    infile.close();
+    outfile.close();
     return (0);
 }
