a727f129a7c984b1fb03b62d07325eb107510a9c
jgravalo <jesusgrawork361@gmail.com>
2023-11-14 17:40:54 +0100

    cpp00 finish

--- 00/ex00/main.cpp
+++ 00/ex00/main.cpp
@@ -12,12 +12,8 @@ int main(int argc, char **argv)
         return (0);
     }
     for (i = 1; argv[i]; i++)
-    {
         for (j = 0; argv[i][j]; j++)
-        {
                 std::cout << (char)toupper(argv[i][j]);
-        }
-    }
     std::cout << std::endl;
     return (0);
 }
