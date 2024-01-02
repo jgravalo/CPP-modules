5d1c98a98153955796fe842ffaa3ece161b72abc
jgravalo <jesusgrawork361@gmail.com>
2024-01-02 14:44:16 +0100

    ex02

--- 02/ex02/src/main.cpp
+++ 02/ex02/src/main.cpp
@@ -4,12 +4,25 @@ int main( void )
 {
     Fixed a;
     Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-/* 
+
+    //a = b;
+    if (a > b)
+        std::cout << "a mayor que b" << std::endl;
+    if (a < b)
+        std::cout << "a menor que b" << std::endl;
+    if (a >= b)
+        std::cout << "a mayor o igual que b" << std::endl;
+    if (a <= b)
+        std::cout << "a menor o igual que b" << std::endl;
+    if (a == b)
+        std::cout << "a igual que b" << std::endl;
+    if (a != b)
+        std::cout << "a distinto de b" << std::endl;
     std::cout << a + b << std::endl;
     std::cout << a - b << std::endl;
     std::cout << a * b << std::endl;
     std::cout << a / b << std::endl;
- */
+ 
     std::cout << a << std::endl;
     std::cout << ++a << std::endl;
     std::cout << a << std::endl;
