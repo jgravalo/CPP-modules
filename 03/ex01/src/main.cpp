68ec48c88fdd8b8087b7dac4585bfdbfeacea173
jgravalo <jesusgrawork361@gmail.com>
2024-01-03 05:58:52 +0100

    ex01

--- 03/ex01/src/main.cpp
+++ 03/ex01/src/main.cpp
@@ -4,16 +4,17 @@ int main()
 {
     ScavTrap    A("Franklin");
     ScavTrap    B(A);
-    //ScavTrap    C;
+    ScavTrap    C;
 
     B.setName("Trevor");
-    //C.setName("Michael");
+    C = A;
+    C.setName("Michael");
+    C.displayInfo();
     A.attack(B.getName());
     B.displayInfo();
     B.takeDamage(A.getAttackDamage());
     B.displayInfo();
     B.beRepaired(2);
     B.displayInfo();
-    A.guardGate();
     return (0);
 }
