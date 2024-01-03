cf16f8a35b8cc24f777bcf1a137947686afc9a4a
jgravalo <jesusgrawork361@gmail.com>
2024-01-03 06:01:30 +0100

    ex02

--- 03/ex02/src/main.cpp
+++ 03/ex02/src/main.cpp
@@ -4,16 +4,17 @@ int main()
 {
     FragTrap    A("Franklin");
     FragTrap    B(A);
-    //FragTrap    C;
+    FragTrap    C;
 
     B.setName("Trevor");
-    //C.setName("Michael");
+    C = A;
+    C.setName("Michael");
+    C.displayInfo();
     A.attack(B.getName());
     B.displayInfo();
     B.takeDamage(A.getAttackDamage());
     B.displayInfo();
     B.beRepaired(2);
     B.displayInfo();
-    A.highFivesGuys();
     return (0);
 }
